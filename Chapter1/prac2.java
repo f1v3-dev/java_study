31cc0c304c02161933d278e99b9402dff61f660f
f1v3-dev <f1v3_dev@naver.com>
2023-06-25 15:17:07 +0900

    Chapter 1

---
+++ Chapter1/prac2.java
@@ -0,0 +1,8 @@
+package Chapter1;
+
+public class prac2 {
+    public static void main(String[] args) {
+        System.out.println("Sorry~~");
+        System.out.println("재밌어서 미안합니다~~");
+    }
+}
